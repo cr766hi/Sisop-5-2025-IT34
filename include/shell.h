4a13130fa471d5c7d7b76c4f856efe2aa2ec483d
Na-ux315 <06naila06@gmail.com>
2025-06-03 21:48:20 +0700

    Update shell.h

--- include/shell.h
+++ include/shell.h
@@ -2,10 +2,26 @@
 #define __SHELL_H__
 
 #include "std_type.h"
+#include "kernel.h"
 
+// Shell core functions
 void shell();
+void printPrompt();
+bool handleCommand(char *buf);
 void parseCommand(char *buf, char *cmd, char arg[2][64]);
 
-// Tambahkan fungsi bila perlu
+// Command handlers
+void changeUsername(char *newName);
+void setGrandCompany(char *company);
+void handleCalculator(char *operation, char *arg1, char *arg2);
+void handleYogurt();
+
+// Utility functions
+void setTextColor(byte color);
+
+// Global variables (extern declarations)
+extern char username[64];
+extern char company[64];
+extern byte textColor;
 
 #endif // __SHELL_H__
