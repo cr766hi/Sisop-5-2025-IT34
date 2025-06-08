b954c14d9ed12718204f01f292efff0324a16250
cr766hi <aldosilalahi765@gmail.com>
2025-06-08 13:21:45 +0700

    Update shell.h

--- include/shell.h
+++ include/shell.h
@@ -1,27 +1,9 @@
-#ifndef __SHELL_H__
-#define __SHELL_H__
+#ifndef SHELL_H
+#define SHELL_H
 
-#include "std_type.h"
 #include "kernel.h"
 
-// Shell core functions
 void shell();
-void printPrompt();
-bool handleCommand(char *buf);
 void parseCommand(char *buf, char *cmd, char arg[2][64]);
 
-// Command handlers
-void changeUsername(char *newName);
-void setGrandCompany(char *company);
-void handleCalculator(char *operation, char *arg1, char *arg2);
-void handleYogurt();
-
-// Utility functions
-void setTextColor(byte color);
-
-// Global variables (extern declarations)
-extern char username[64];
-extern char company[64];
-extern byte textColor;
-
-#endif // __SHELL_H__
+#endif
